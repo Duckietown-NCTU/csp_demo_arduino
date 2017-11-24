6acfabe6b9ee4041856bc532ed3f40fe4886545b
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-11-25 01:40:09 +0800

    update

--- src/wheel_node.py
+++ src/wheel_node.py
@@ -3,7 +3,7 @@ import rospy
 import numpy as np
 from duckietown_msgs.msg import BoolStamped, Twist2DStamped
 
-class arduinoROS(object):
+class arduinoWheel(object):
     def __init__(self):
         # =========== publisher ===========
         self.pub_car_cmd = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
@@ -21,6 +21,6 @@ class arduinoROS(object):
             print "go backward"
 
 if __name__ == "__main__":
-    rospy.init_node("arduino_ros", anonymous = False)
-    arduino_node = arduinoROS()
+    rospy.init_node("arduino_wheel", anonymous = False)
+    arduino_node = arduinoWheel()
     rospy.spin()
